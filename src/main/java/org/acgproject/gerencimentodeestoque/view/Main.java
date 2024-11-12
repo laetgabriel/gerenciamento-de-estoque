7e9edc2277b6e30a5284ada1a4ab52ff1832d57f
Brunno Pereira <cicerobrnn111@gmail.com>
2024-11-11 21:17:46 -0300

    Adicionando interfaces

--- src/main/java/org/acgproject/gerencimentodeestoque/view/Main.java
+++ src/main/java/org/acgproject/gerencimentodeestoque/view/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/acgproject/gerencimentodeestoque/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = FXMLLoader.load(getClass().getResource("/org/acgproject/gerencimentodeestoque/view/Categoria.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Gerenciamento Estoque");
         stage.setScene(scene);
         stage.show();
     }
