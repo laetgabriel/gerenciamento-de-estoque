4e8d6ef027a433121a7f699c7f2c01165eed5503
Brunno Pereira <cicerobrnn111@gmail.com>
2024-11-11 22:54:33 -0300

    Deixando interfaces redimensionáveis

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module org.acgproject.gerencimentodeestoque {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens org.acgproject.gerencimentodeestoque to javafx.fxml;
-    exports org.acgproject.gerencimentodeestoque.view;
+    // Abre o pacote 'view' para reflexão, necessário para carregar FXML
     opens org.acgproject.gerencimentodeestoque.view to javafx.fxml;
-    exports org.acgproject.gerencimentodeestoque.view.controller;
-    opens org.acgproject.gerencimentodeestoque.view.controller to javafx.fxml;
+    // Exporta o pacote 'view' para que outras partes do projeto possam acessá-lo
+    exports org.acgproject.gerencimentodeestoque.view;
+
+    // Exporta e abre o pacote 'controller' para o JavaFX acessar os controladores
+    exports org.acgproject.gerencimentodeestoque.controller;
+    opens org.acgproject.gerencimentodeestoque.controller to javafx.fxml;
+
 }
--- src/main/java/org/acgproject/gerencimentodeestoque/view/App.java
+++ src/main/java/org/acgproject/gerencimentodeestoque/view/App.java
@@ -2,18 +2,28 @@ package org.acgproject.gerencimentodeestoque.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = FXMLLoader.load(getClass().getResource("/org/acgproject/gerencimentodeestoque/view/Categoria.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/acgproject/gerencimentodeestoque/view" +
+                "/Categoria.fxml"));
+
+        ScrollPane scroll = loader.load();
+
+        scroll.setFitToWidth(true);
+        scroll.setFitToHeight(true);
+
+        Scene scene = new Scene(scroll);
         stage.setTitle("Gerenciamento Estoque");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
