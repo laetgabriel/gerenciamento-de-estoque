033ab0d402e94a432ac7fb2dce0dd0f70b7bce71
Anderson da Silva Santos <anderson.ss1090@gmail.com>
2024-11-15 19:16:15 -0300

    adição da validação de fornecedor na tela cadastro e botão de salvar configurado

--- src/main/java/org/acgproject/gerencimentodeestoque/controller/FornecedorController.java
+++ src/main/java/org/acgproject/gerencimentodeestoque/controller/FornecedorController.java
@@ -3,13 +3,16 @@ package org.acgproject.gerencimentodeestoque.controller;
 import org.acgproject.gerencimentodeestoque.dao.FornecedorDAO;
 import org.acgproject.gerencimentodeestoque.dao.impl.FornecedorDAOImpl;
 import org.acgproject.gerencimentodeestoque.dto.FornecedorDTO;
+import org.acgproject.gerencimentodeestoque.model.entities.Fornecedor;
 
 import java.util.List;
 
 public class FornecedorController {
 
     private FornecedorDAO fornecedorDAO = new FornecedorDAOImpl();
-
+    public void inserirFornecedor(FornecedorDTO fornecedorDTO) {
+        fornecedorDAO.inserirFornecedor(fornecedorDTO);
+    }
     public FornecedorDTO consultarFornecedorPorNome(String nome){return fornecedorDAO.consultarFornecedorPorNome(nome);}
     public List<FornecedorDTO> listarTodosOsFornecedores() { return fornecedorDAO.listarTodosOsFornecedores();}
 }
